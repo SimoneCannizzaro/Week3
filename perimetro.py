3517b4d7987425d088ec7f3265cd5e174eade587
SimoneCannizzaro <109146607+SimoneCannizzaro@users.noreply.github.com>
2022-07-18 16:35:34 +0200

    Add files via upload

--- perimetro.py
+++ perimetro.py
@@ -1,14 +1,14 @@
-def perimetro():
+def perimetro(): #def definisce una funzione
 	print("Il programma calcola il perimetro di una figura geometrica")
 	print("""
 	- Quadrato>>1
 	- Rettangolo>>2
 	- Cerchio>>3
-	- Triangolo>>4""")
+	- Triangolo Scaleno>>4""")
 
-	print("Inserire la scelta: ")
+	print("Inserire la scelta: ") #inizio del programma
 	scelta = int(input(">>> "))
-	if scelta == 1:
+	if scelta == 1: #a secondo del numero inserito si visualizzerà il perimetro della figura scelta 
 		print("Hai selezionato il perimetro del Quadrato")
 		lato = int(input("inserisci il valore del lato quadrato "))
 		print("Il perimetro del Quadrato, avente lato", lato, "è:", lato*4)
@@ -23,11 +23,11 @@ def perimetro():
 		diametro = int(raggio*2)
 		print("Il perimetro del Cerchio, avente raggio", raggio, "è:", diametro*3.14)
 	elif scelta == 4:
-		print("Hai selezionato il perimetro del Triangolo")
+		print("Hai selezionato il perimetro del Triangolo Scaleno")
 		latoa = int(input("Inserisci il valore lato a "))
 		latob = int(input("Inserisci il valore lato b "))
 		latoc = int(input("Inserisci il valore lato c "))
-		print("Il perimetro del Triangolo, avente lato a", latoa, " lato b", latob, " e lato c",latoc, "è:", latoa+latob+latoc)
-	else:
+		print("Il perimetro del Triangolo Scaleno, avente lato a", latoa, " lato b", latob, " e lato c",latoc, "è:", latoa+latob+latoc)
+	else:            #se non viene inserito nessun numero tra le scelte apparirà questo messaggio
 		print("Inserire una scelta valida")
-perimetro()
+perimetro()               #fine funzione
